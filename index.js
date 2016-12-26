7c1ac618341055bc3b4f892ca69436ce2a4e1a59
felixacdc <pfmata01@gmail.com>
2016-12-26 10:14:29 -0600

    [add] 01 NodeJs Hapi Route Parameters

--- index.js
+++ index.js
@@ -2,17 +2,41 @@ const Hapi = require('hapi');
 
 const server = new Hapi.Server();
 
-server.connection({
-    host: 'localhost',
-    port: 8000
-});
+server.connection({ port: 8000 });
+
+function handler(request, reply) {
+    reply(request.params);
+}
 
 server.route({
     method: 'GET',
-    path: '/',
-    handler: (request, reply) => {
-        reply('hello hapi');
-    }
+    path: '/users/{userId}',
+    handler: handler
 });
+
+// route parameters
+// opcionales
+/*server.route({
+    method: 'GET',
+    path: '/users/{userId?}',
+    handler: handler
+});*/
+// cantidad de parametros ilimitada
+/*server.route({
+    method: 'GET',
+    path: '/files/{files*}',
+    handler: handler
+});*/
+// cantidad de parametros limitada
+/*server.route({
+    method: 'GET',
+    path: '/files/{files*2}',
+    handler: handler
+});*/
+/*server.route({
+    method: 'GET',
+    path: '/files/{file}.jpg',
+    handler: handler
+});*/
 
 server.start(() => console.log(`Started at: ${server.info.uri}`));
